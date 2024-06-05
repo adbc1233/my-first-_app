d2dc3d47927796a50a747914f0eaa503ab90a335
adbc1233 <shushmat2001@gmail.com>
2024-06-05 15:43:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title('Simple Streamlit App')
+
+st.write('This is a simple Streamlit app that prints a single paragraph of text.')
+st.write('Streamlit makes it easy to create and share data apps with just a few lines of Python code.')
+
+st.text('This is a simple Streamlit app that prints a single paragraph of text.Streamlit makes it easy to create and share data apps with just a few lines of Python code.')
